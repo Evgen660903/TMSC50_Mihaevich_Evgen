3f138eb24ec56c73fbc4e064513597e9dfeb1bc8
Evgen660903 <emihaevich@gmail.com>
2021-09-29 11:49:33 +0300

    init Git

--- src/homework1/Task3.java
+++ src/homework1/Task3.java
@@ -3,6 +3,7 @@ package homework1;
 import java.util.Scanner;
 
 public class Task3 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите пожалуйста количество дней");
